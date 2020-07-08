627f7c7722d96028eca98ac07aeb0b46a461557b
meredithparks <mereditheparks@gmail.com>
2020-07-07 20:10:42 -0600

    updated picture and tested

--- db/schema.sql
+++ db/schema.sql
@@ -3,6 +3,8 @@ CREATE DATABASE burgers_db;
 
 USE burgers_db;
 
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'Duchess143!';
+
 CREATE TABLE burgers (
 	id INT (10) AUTO_INCREMENT NOT NULL,
     burger_name VARCHAR (100) NOT NULL,
